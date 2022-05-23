3540112611d87225834afa64fa8c6bbe8253fe10
Michał Przytarski <michal.kacper.przytarski@gmail.com>
2022-05-23 07:56:26 +0200

    Initial files

---
+++ studentA.py
@@ -0,0 +1,6 @@
+def print_board:
+    pass
+def is_game_over:
+    pass
+def new_board:
+    pass
---
+++ studentB.py
@@ -0,0 +1,6 @@
+def ai_move:
+    pass
+def get_user_move:
+    pass
+def is_player_starting:
+    pass 
